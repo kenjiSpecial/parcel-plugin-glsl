01e513e19f72b578dde6af58e125a24a6ff1df82
Kenji <k.saito.1985@gmail.com>
2018-01-09 14:22:37 +0100

    support vert, vertx, frag, fragment formats.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-module.exports = function (bundler) {
-
+module.exports = function(bundler) {
   bundler.addAssetType('glsl', require.resolve('./glsl-asset'));
-
+  bundler.addAssetType('vert', require.resolve('./glsl-asset'));
+  bundler.addAssetType('vertex', require.resolve('./glsl-asset'));
+  bundler.addAssetType('fragment', require.resolve('./glsl-asset'));
+  bundler.addAssetType('frag', require.resolve('./glsl-asset'));
 };
