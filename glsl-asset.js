4e78625a45a9892699e1c5aa46b2bd8783372c1b
Kenji Saito <k.saito.1985@gmail.com>
2018-01-12 16:11:05 +0100

    Update glsl-asset.js

--- glsl-asset.js
+++ glsl-asset.js
@@ -1,6 +1,7 @@
 const { Asset } = require('parcel-bundler');
+const JSAsset = require('parcel-bundler/src/assets/JSAsset');
 
-module.exports = class GLSLAsset extends Asset {
+module.exports = class GLSLAsset extends JSAsset {
   constructor(name, pkg, options) {
     super(name, pkg, options);
     this.type = 'js';
